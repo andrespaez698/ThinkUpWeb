c6b6db0e2eac346aca274089f94db91cd1e92770
andrespaez698 <andres1865@hotmail.com>
2016-11-28 14:54:58 -0500

    Cargando archivo

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+ <meta charset="UTF-8" />
+ <link rel="stylesheet"  href="css/estilos.css" />
+ <link rel="stylesheet" type="text/css" href="fonts/style.css">
+ <title>THINKUP</title>
+</head>
+<body>
+<header>
+<div id="BarraSup">
+      <div id="menulog"><span class="icon-menu"></span><label id="tituloPag">Perfil</label><input id="busquedatxt" type="text" name="busqueda"><div id="menulog1"><span class="icon-dots-three-vertical"></span></div><div id="menulog1"><span class="icon-magnifying-glass"></span></div></div>
+      
+</div>
+ </header>
+ 
+<div id="Menu">      
+         <div class="avatar" style="background-image: url(images/perfil.png)">
+           </div>
+            <div id="nombreUsuario">
+            <label>apaez@hotmail.com</label>
+            </div>
+
+
+          <ul id="menu_principal">
+            <li id="lista"><a href="#"><span class="icon-home"></span> Inició</a></li>
+            <li id="lista"><a href="#"><span class="icon-users"></span> Contactos</a></li>
+            <li id="lista"><a href="#"><span class="icon-cloud"></span> Comunidad</a></li>
+          </ul>       
+</div>
+<div id="contenido">
+	<div id="dash">
+		<div class="contder carta-1">
+		<div class="cartaImagen">
+        <img src="images/grupos.png">
+        </div>
+        <div>MIS GRUPOS</div>
+		</div>
+		<div class="contizq carta-1">
+		<div class="cartaImagen">
+        <img src="images/idea.png">
+        </div>
+        <DIV>MIS IDEAS</DIV>
+		</div>
+	</div>
+	
+</div>
+</body>
+</html>
---
+++ misideas.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+ <meta charset="UTF-8" />
+ <link rel="stylesheet"  href="css/estilos.css" />
+ <link rel="stylesheet" type="text/css" href="fonts/style.css">
+ <title>THINKUP</title>
+</head>
+<body>
+<header>
+<div id="BarraSup">
+      <div id="menulog"><span class="icon-menu"></span><label id="tituloPag">Mis Ideas</label><input id="busquedatxt" type="text" name="busqueda"><div id="menulog1"><span class="icon-dots-three-vertical"></span></div><div id="menulog1"><span class="icon-magnifying-glass"></span></div></div>
+      
+</div>
+ </header>
+ 
+<div id="Menu">      
+         <div class="avatar" style="background-image: url(images/perfil.png)">
+           </div>
+            <div id="nombreUsuario">
+            <label>apaez@hotmail.com</label>
+            </div>
+
+
+          <ul id="menu_principal">
+            <li id="lista"><a href="#"><span class="icon-home"></span> Inició</a></li>
+            <li id="lista"><a href="#"><span class="icon-users"></span> Contactos</a></li>
+            <li id="lista"><a href="#"><span class="icon-cloud"></span> Comunidad</a></li>
+          </ul>       
+</div>
+<div id="contenido">
+	<div id="dash1">
+  MIS IDEAS</br>
+	AQUI ESTA UNA LISTA DE NUESTRAS IDEAS QUE SE ACTUALIZARAN AL HACER LA CONEXION CON LA BASE DE DATOS 	
+	</div>
+	</div>
+	
+</div>
+</body>
+</html>
